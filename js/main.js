54894a58010e9ed5d6d02d6bc0ab29ca88e27980
David Kamphausen <david.kamphausen@web.de>
2014-03-27 20:55:47 +0100

    timeout up

--- js/main.js
+++ js/main.js
@@ -7,31 +7,31 @@ require.config({
     "backbone": "libs/backbone/backbone",
     "localstorage": "libs/backbone.localstorage/backbone.localStorage",
     "mustache": "libs/mustache/mustache",
-//    "bootstrap": "libs/bootstrap/dist/js/bootstrap",
+    "bootstrap": "libs/bootstrap/dist/js/bootstrap",
     "hide-address-bar": "libs/hide-address-bar/hide-address-bar",
     "fastclick": "libs/fastclick-amd/fastclick"
   },
   shim: {
     "backbone": {
       //   loads dependencies first
-      deps: ["jquery", "underscore"]
+      deps: ["jquery", "underscore"],
       // custom export name, this would be lowercase otherwise
-//      exports: "Backbone"
+      exports: "Backbone"
     },
     "bootstrap": {
-      deps:["jquery"]
- //     exports:"Bootstrap"
+      deps:["jquery"],
+      exports:"Bootstrap"
     }
   },
   //  how long the it tries to load a script before giving up, the default is 7
-  waitSeconds: 10
+  waitSeconds: 30
 });
 //requiring the scripts in the first argument and then passing the library namespaces into a callback
 //you should be able to console log all of the callback arguments
 //require(['bootstrap,jquery', 'underscore', 'backbone', 'hide-address-bar'], function(jquery, _, Backbone,hideAddressBar){
 //require(['bootstrap', 'app'], function(Bootstrap, App){
 
-require(['backbone','route'], function( Backbone,App){
+require(['bootstrap','backbone','route'], function(Bootstrap, Backbone, App){
   new App;
   Backbone.history.start();
 });
