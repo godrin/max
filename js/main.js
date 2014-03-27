28c86feac570942a253c97a3f56d2657e36940f8
David Kamphausen <david.kamphausen@web.de>
2014-03-27 20:04:35 +0100

    r.js deps fixed maybe

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ require.config({
 //require(['bootstrap,jquery', 'underscore', 'backbone', 'hide-address-bar'], function(jquery, _, Backbone,hideAddressBar){
 //require(['bootstrap', 'app'], function(Bootstrap, App){
 
-require(['bootstrap', 'route'], function(Bootstrap, App){
+require(['bootstrap', 'backbone','route'], function(Bootstrap, Backbone,App){
   new App;
   Backbone.history.start();
 });
