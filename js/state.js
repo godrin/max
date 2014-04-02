677cd21756ef5f1517d50159ef4afad74ebe6732
David Kamphausen <david.kamphausen@web.de>
2014-04-02 07:38:56 +0200

    small cleanup

--- js/state.js
+++ js/state.js
@@ -1,12 +1,9 @@
 define(["backbone","text!levels.json","localstorage"],function(Backbone,levelsData,localStorage) {
-//define(["backbone","localstorage","text!levels.json"],function(Backbone,levelsData) {
-
   var StateModel=Backbone.Model.extend({
   });
   var StateCollection=Backbone.Collection.extend({
     localStorage: new Backbone.LocalStorage("state"), // Unique name within your app.
     upsert:function(data) {
-//      data.id=data.url;
       console.log("UPSERT",data);
       var s=this.findWhere({url:data.url});
       if(s) {
@@ -28,7 +25,6 @@ define(["backbone","text!levels.json","localstorage"],function(Backbone,levelsDa
     model: LevelModel,
     url:"js/levels.json"
   });
-  console.log("DATA",levelsData);
   var LevelCollectionInstance=new LevelCollection(JSON.parse(levelsData));
   return {
     LevelModel:LevelModel,
